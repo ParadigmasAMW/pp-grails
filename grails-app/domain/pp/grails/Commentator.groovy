0e83a1c2ff19caf7d724f33b0fa0326a4f481f4b
Álvaro Fernando <alvarofernandoms@gmail.com>
2015-04-03 14:41:38 -0300

    Update Commentador class
    Signed-off-by: Álvaro Fernando <alvarofernandoms@hotmail.com>
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Wilker Mesquita <wilker-mc@hotmail.com>

--- grails-app/domain/pp/grails/Commentator.groovy
+++ grails-app/domain/pp/grails/Commentator.groovy
@@ -2,6 +2,16 @@ package pp.grails
 
 class Commentator {
 
+	static belongsTo = Comment
+
+	String name
+	String url
+	String email
+	Comment comment
+
     static constraints = {
+    	name(nullable: false, blank: false)
+    	url(nullable: true, blank: true, url: true)
+    	email(nullable: true, blank: true, email: true)
     }
 }
